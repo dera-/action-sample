04dd27e27112f45e4a36749cb75afa5d86fecb35
dera- <michihiro_onodera@dwango.co.jp>
2025-11-26 19:52:59 +0900

    add only_pull_request option

--- release/src/index.js
+++ release/src/index.js
@@ -7,6 +7,7 @@ const generateReleaseNote = require("./generateReleaseNote");
 const inputs = {
 	githubToken: core.getInput("github_token"),
 	npmToken: core.getInput("npm_token"),
+	onlyPullRequest: core.getInput("only_pull_request"),
 };
 const targetDirPath = process.env.GITHUB_WORKSPACE;
 const packageJsonPath = path.join(targetDirPath, "package.json");
@@ -16,9 +17,31 @@ const repositoryInfo = process.env.GITHUB_REPOSITORY.split("/");
 const ownerName = repositoryInfo[0];
 const repositoryName = repositoryInfo[1];
 const gitCommitHash = process.env.GITHUB_SHA;
+const currentBranch = process.env.GITHUB_REF_NAME;
 
 (async () => {
 	try {
+		const octokit = github.getOctokit(inputs.githubToken);
+		if (inputs.onlyPullRequest) {
+			const branchName = `update-changelog-for-${Date.now()}`;
+			await git.checkoutLocalBranch(branchName);
+			const newfilePath = path.join(targetDirPath, "test.txt");
+			fs.writeFileSync(newfilePath, "date:" + Date.now());
+			await git.add(newfilePath);
+			await git.commit("add test.txt");
+			await git.push("origin", branchName);
+			const prTitle = `Add file`;
+			const prBody = `Add file.\n\nPlease check the contents and merge this Pull Request to proceed with the release.`;
+			await octokit.rest.pulls.create({
+				owner: ownerName,
+				repo: repositoryName,
+				title: prTitle,
+				body: prBody,
+				head: branchName,
+				base: currentBranch
+			});
+			return;
+		}
 		await npmPublish({
 			package: packageJsonPath,
 			token: inputs.npmToken
@@ -30,7 +53,6 @@ const gitCommitHash = process.env.GITHUB_SHA;
 			const changelog = fs.readFileSync(changelogPath).toString();
 			body = generateReleaseNote(changelog, version);
 		}
-		const octokit = github.getOctokit(inputs.githubToken);
 		await octokit.repos.createRelease({
 			owner: ownerName,
 			repo: repositoryName,
